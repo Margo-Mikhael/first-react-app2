0b5886f4830d812df534e8aadfd8e49e848c7021
Margo-Mikhael <margomikhaell@gmail.com>
2025-12-22 01:15:21 +0200

    Add SVG avatar asset and refactor Header component layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,29 @@
+import { Star } from "lucide-react";
 import React from "react";
 
 export default function Header({ text, isDark }) {
   return (
-    <h1
-      className={`text-lg font-bold uppercase text-[40px] ${
-        isDark ? "text-default" : "text-white"
-      }`}
-    >
-      {text}
-    </h1>
+    <div className="flex flex-col items-center">
+      <h1
+        className={`text-lg font-bold uppercase text-[40px] ${
+          isDark ? "text-default" : "text-white"
+        }`}
+      >
+        {text}
+      </h1>
+      {/* Star Section */}
+      <div className="flex gap-4 items-center">
+        <span
+          className={`h-1 w-20 ${isDark ? "bg-default" : "bg-white"}`}
+        ></span>
+        <Star
+          fill={isDark ? "currentColor" : "#fff"}
+          className={`size-4 ${isDark ? "text-default" : "text-white"}`}
+        />
+        <span
+          className={`h-1 w-20 ${isDark ? "bg-default" : "bg-white"}`}
+        ></span>
+      </div>
+    </div>
   );
 }
